3f8a3a6b775fe095f9ae9309208d47c7c0a42326
joepak <josephpak2014@gmail.com>
2019-03-04 15:35:05 -0500

    Increment Async Stretch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,12 +11,12 @@ class Counter extends Component {
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(() => {
+            this.props.increment(this.props.count)
+        }, 1000)
     };
 
     render() {
-        // Fill in the two button onClick methods
-        // Upon clicking these buttons, the count
-        // should decrement or increment accordingly
         return (
             <p>
                 Clicked: {this.props.count} times
@@ -26,14 +26,13 @@ class Counter extends Component {
                 <button onClick={() => {this.props.decrement(this.props.count) }}>
                     -
                 </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
